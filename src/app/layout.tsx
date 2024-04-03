f11021a7781da63dafc3aeaa053441bf001de578
Paulo Braga Costa <paulobragacosta@hotmail.com>
2024-04-03 14:58:42 -0300

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
+// layout.js
 import "@/styles/globals.css"
-import { Inter as FontSans } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
-
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  // You can use fontSans here if needed
   return (
     <>
       <html lang="en" suppressHydrationWarning>
