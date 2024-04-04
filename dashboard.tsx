3a3c0dee7c8616a8b2a4ece8eb519bd5a807eab4
Paulo Braga Costa <paulobragacosta@hotmail.com>
2024-04-04 09:21:55 -0300

    update 5

--- dashboard.tsx
+++
@@ -1,5 +0,0 @@
-export default function Dashboard() {
-    return (
-      <h2>Deu certo o hello word</h2>
-    );
-  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,41 @@
-"use client";
+import { auth, signIn, signOut } from "../../auth";
 
-import Link from "next/link";
-
-export default function App() {
+function SignIn() {
   return (
-    <div>
-    <nav>
-      <Link href="/signin">entrar</Link>
-    </nav>
-    
-    </div>
+    <form
+      action={async () => {
+        "use server";
+        await signIn("github");
+      }}
+    >
+      <p>You are not logged in</p>
+      <button type="submit">Sign in with GitHub</button>
+    </form>
+  );
+}
+
+function SignOut({ children }: { children: React.ReactNode }) {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      <p>{children}</p>
+      <button type="submit">Sign out</button>
+    </form>
+  );
+}
+
+export default async function Page() {
+  let session = await auth();
+  let user = session?.user?.email;
+
+  return (
+    <section>
+      <h1>Home</h1>
+      <div>{user ? <SignOut>{`Welcome ${user}`}</SignOut> : <SignIn />}</div>
+    </section>
   );
 }
