4922fbf4b06e7b03bd9fdb3ac23c354bdf8be8da
Paulo Braga Costa <paulobragacosta@hotmail.com>
2024-08-16 13:35:41 -0300

    update

--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -203,7 +203,7 @@ export default function Home() {
               ) : (
                 <>
                   <Plus className="mr-2 h-4 w-4" />
-                  salvar
+                  salvarr
                 </>
               )}
             </Button>
