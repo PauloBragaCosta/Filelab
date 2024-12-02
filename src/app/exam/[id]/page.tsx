01bf17d1d360813313f3b683e196f2647ec26797
Paulo Braga Costa <paulobragacosta@hotmail.com>
2024-12-02 14:51:19 -0300

    exame

---
+++ src/app/exam/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function exam() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Página em Branco</h1>
+    </main>
+  )
+}
+
